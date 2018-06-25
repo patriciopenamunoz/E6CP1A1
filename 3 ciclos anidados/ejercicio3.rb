77368f49ddbc084370c5f3c1ba6b55bdc5b1c140
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-24 22:32:31 -0400

    Resolver ejercicio3.rb

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,27 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+salir = false
+
+until salir
+
+  puts 'Ingrese un valor o ingrese cero para salir:'
+
+  largo = gets.chomp.to_i
+
+  if largo.zero?
+    salir = true
+  else
+    output = ''
+    largo.times do |i|
+      i += 1
+      largo.times do |e|
+        e += 1
+        output += "#{i * e}\t"
+      end
+      output += "\n"
+    end
+  end
+  puts output
+end
